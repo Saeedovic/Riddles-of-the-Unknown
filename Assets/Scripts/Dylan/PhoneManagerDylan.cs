ad82b00af081444074eedec5fd18a6fb893234e7
Dylan Suaris <dylansuaris@gmail.com>
2023-03-27 04:53:48 +0400

    Pushing PhoneSystem Prototype 2

--- Assets/Scripts/Dylan/PhoneManagerDylan.cs
+++ Assets/Scripts/Dylan/PhoneManagerDylan.cs
@@ -16,10 +16,36 @@ public class PhoneManagerDylan : MonoBehaviour
 
     public GameObject backButton;
 
+    public GameObject MainCamera;
+    public GameObject MainCamFollow;
+
+    public GameObject PhoneCamera;
+    public GameObject PhoneCamFollow;
+
+    public Transform camPoint;
+
+    public GameObject phone;
+    public GameObject OriginalPhonePos;
+
+
+    private void Awake()
+    {
+        
+    }
+
 
     // Start is called before the first frame update
     void Start()
     {
+        sP = GetComponent<FirstPersonController>();
+
+        MainCamera.SetActive(true);
+        MainCamFollow.SetActive(true);
+
+        PhoneCamera.SetActive(false);
+        PhoneCamFollow.SetActive(false);
+
+
         if (sP._mobilePhone.activeInHierarchy == true)
         {
             _App1.SetActive(false);
@@ -44,9 +70,32 @@ public class PhoneManagerDylan : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if(_App1.activeInHierarchy == false && _App2.activeInHierarchy == false && _App3.activeInHierarchy == false && _App4.activeInHierarchy == false && _App5.activeInHierarchy == false && _App6.activeInHierarchy == false)
         {
             backButton.SetActive(false);
+        }
+
+        if (_App1.activeInHierarchy == true || _App2.activeInHierarchy == true || _App3.activeInHierarchy == true || _App4.activeInHierarchy == true || _App5.activeInHierarchy == true || _App6.activeInHierarchy == true)
+        {
+
+            MainCamera.SetActive(false); 
+            MainCamFollow.SetActive(false); 
+            
+            PhoneCamera.SetActive(true);
+            PhoneCamFollow.SetActive(true);
+
+
+            phone.transform.position = camPoint.transform.position +  new Vector3(0.4f , 0, 0.4f);
+
+        }
+        else
+        {
+            MainCamera.SetActive(true);
+            MainCamFollow.SetActive(true);
+
+            PhoneCamera.SetActive(false);
+            PhoneCamFollow.SetActive(false);
         }
     }
 
@@ -95,12 +144,19 @@ public class PhoneManagerDylan : MonoBehaviour
 
     public void BackButton()
     {
+
+
+        phone.transform.position = OriginalPhonePos.transform.position;
+
+
         _App1.SetActive(false);
         _App2.SetActive(false);
         _App3.SetActive(false);
         _App4.SetActive(false);
         _App5.SetActive(false);
         _App6.SetActive(false);
+
+
     }
 
     
