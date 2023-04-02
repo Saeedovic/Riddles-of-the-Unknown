f202794fb2a7eb555d511748d83d3c35482dafd0
Dylan Suaris <dylansuaris@gmail.com>
2023-04-03 01:23:40 +0400

    Merged Leveling Up and Stat Allocation App with Shamma's Latest Work!
    Merged Work into ShammaInteractionTest Scene

--- Assets/Scripts/Shamma/PlayerMovementController.cs
+++ Assets/Scripts/Shamma/PlayerMovementController.cs
@@ -6,16 +6,28 @@ public class PlayerMovementController : MonoBehaviour
 {
     CharacterController playerController;
     public float moveIntensity = 1f;
+    XPManager xP;
 
     // Start is called before the first frame update
     void Start()
     {
         playerController = GetComponent<CharacterController>();
+        xP = GetComponent<XPManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //-----Dylan------//
+
+        //TO ADD XP to Player's Xp variable
+        if (Input.GetKey(KeyCode.L))
+        {
+            xP.AddXp(10);
+        }
+
+        //-----Dylan------//
+
         Vector3 moveInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         Vector3 moveDir = transform.TransformDirection(moveInput);
         playerController.Move(moveDir * moveIntensity);
