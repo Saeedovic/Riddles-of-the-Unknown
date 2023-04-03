014f3eeaac91ed826936631ef8e1ba56402e947d
seapuff <s.albreiki@student.saedubai.com>
2023-04-03 19:48:02 +0400

    Added ecopoint prototype
    -wrote a script that checks a radius when the player presses, O and adds an outline shader to any ecopoint objects within.

--- Assets/Scripts/Shamma/PointOfInterest.cs
+++ Assets/Scripts/Shamma/PointOfInterest.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 public class PointOfInterest : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // empty class for ecopoint objects to inherit from.
+    // may convert this to an interface? depends what works best
 }
