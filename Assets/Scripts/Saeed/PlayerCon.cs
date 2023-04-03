f21f39b1cc4083fbee6a173b13b469ddc3279718
Saeedovic <Seedxlss@gmail.com>
2023-04-04 01:13:41 +0400

    Survival System prototype 2 works now perfectly

--- Assets/Scripts/Saeed/PlayerCon.cs
+++ Assets/Scripts/Saeed/PlayerCon.cs
@@ -5,28 +5,67 @@ using UnityEngine;
 public class PlayerCon : MonoBehaviour
 {
     public float speed = 5f;
+    public float walkSpeed = 5f;
+    public float runSpeed = 10f;
     public float jumpForce = 10f;
     public float gravity = -20f;
+    public float sensitivity = 100f;
+    public Transform cameraTransform;
+    public KeyCode runKey = KeyCode.LeftShift;
 
     private CharacterController controller;
     private Vector3 playerVelocity;
+    private float mouseX = 0f;
+    private float mouseY = 0f;
+    public bool isRunning = false;
 
     private void Start()
     {
         controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
+
     private void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+        mouseX += Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        mouseY -= Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+        mouseY = Mathf.Clamp(mouseY, -90f, 90f);
+
+        cameraTransform.localRotation = Quaternion.Euler(mouseY, 0f, 0f);
+        transform.rotation = Quaternion.Euler(0f, mouseX, 0f);
 
         Vector3 move = transform.right * horizontal + transform.forward * vertical;
-        controller.Move(move * speed * Time.deltaTime);
+
+        float currentSpeed;
+        if (isRunning)
+        {
+            currentSpeed = runSpeed;
+        }
+        else
+        {
+            currentSpeed = walkSpeed;
+        }
+
+
+        controller.Move(move * currentSpeed * Time.deltaTime);
         playerVelocity.y += gravity * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
+
         if (controller.isGrounded && Input.GetButtonDown("Jump"))
         {
             playerVelocity.y = Mathf.Sqrt(jumpForce * -2f * gravity);
+        }
+
+        if (Input.GetKey(runKey))
+        {
+            isRunning = true;
+        }
+
+        if (Input.GetKeyUp(runKey))
+        {
+            isRunning = false;
         }
     }
 }
--- Assets/Scripts/Saeed/StaminaSystem.cs
+++ Assets/Scripts/Saeed/StaminaSystem.cs
@@ -10,46 +10,44 @@ public class StaminaSystem : MonoBehaviour
     public float staminaIncreaseRate = 20f;
 
     public float currentStamina;
-    private bool isMoving;
     private CharacterController controller;
     public Slider staminaBar;
+    private PlayerCon playerCon;
 
     private void Start()
     {
         currentStamina = maxStamina;
         controller = GetComponent<CharacterController>();
         staminaBar = FindObjectOfType<Slider>();
-       // UpdateStaminaBar();
+        playerCon = GetComponent<PlayerCon>();
+        UpdateStaminaBar();
     }
 
     private void Update()
     {
-        isMoving = (Mathf.Abs(Input.GetAxis("Horizontal")) > 0f || Mathf.Abs(Input.GetAxis("Vertical")) > 0f);
+        bool isRunning = playerCon.isRunning;
 
-        if (isMoving && currentStamina > 0f)
+        if (isRunning && currentStamina > 0f)
         {
             currentStamina -= staminaDecreaseRate * Time.deltaTime;
             if (currentStamina <= 0f)
             {
                 currentStamina = 0f;
-                isMoving = false;
+                isRunning = false;
             }
             UpdateStaminaBar();
         }
-        else if (!isMoving && currentStamina < maxStamina)
-        {       
+        else if (!isRunning && currentStamina < maxStamina)
+        {
             currentStamina += staminaIncreaseRate * Time.deltaTime;
             if (currentStamina > maxStamina)
             {
                 currentStamina = maxStamina;
             }
-
-          
             UpdateStaminaBar();
         }
 
-        
-        if (isMoving && currentStamina > 0f)
+        if (isRunning && currentStamina > 0f)
         {
             float horizontal = Input.GetAxis("Horizontal");
             float vertical = Input.GetAxis("Vertical");
@@ -57,23 +55,22 @@ public class StaminaSystem : MonoBehaviour
             Vector3 move = transform.right * horizontal + transform.forward * vertical;
             controller.Move(move * Time.deltaTime);
 
-           
             currentStamina -= move.magnitude * staminaDecreaseRate * Time.deltaTime;
 
             if (currentStamina <= 0f)
             {
                 currentStamina = 0f;
-                isMoving = false;
+                isRunning = false;
             }
 
-         
             UpdateStaminaBar();
         }
+
         Debug.Log("current stamina:" + currentStamina);
     }
 
     public void UpdateStaminaBar()
     {
-        staminaBar.value = currentStamina / maxStamina;
+        staminaBar.value = currentStamina;
     }
 }
