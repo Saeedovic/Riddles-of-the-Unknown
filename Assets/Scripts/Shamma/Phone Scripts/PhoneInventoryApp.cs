425ab946a8395a4d02daa5549775ba33e21cf0a4
seapuff <s.albreiki@student.saedubai.com>
2023-06-21 23:43:26 +0400

    made a working use / drop popup for the inventory app!
    -added a confirmation box for inventory which has buttons for options to use an item, or drop the item. (changes were saved to player prefab)
    -added script for inventory app, controls the confirmation box and which item is being used / discarded.
    -also duplicated ecopoint highlight shader a few times for different colours on different type objects.
    -still need to fix the inventory icons... working on it
    
    to do:
    -fix fullscreen position
    -set up ecopoint to only work in camera app fullscreen

--- Assets/Scripts/Shamma/Phone Scripts/PhoneInventoryApp.cs
+++ Assets/Scripts/Shamma/Phone Scripts/PhoneInventoryApp.cs
@@ -1,18 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class PhoneInventoryApp : MonoBehaviour
+public class PhoneInventoryApp : PhoneAppScreen
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject confirmBox; // object that slots use to execute the drop or use funcs
+    [SerializeField] Button confUseButton;
+    [SerializeField] Button confDropButton;
+    [SerializeField] Button confBackButton; // buttons in the confirm box
+    InventorySlot currentSlot; // temporarily saved item, for the confirmation box to reference
+
+    public override void OnOpenApp()
     {
-        
+        if (confirmBox.activeInHierarchy)
+        {
+            confirmBox.SetActive(false);
+        }
+        base.OnOpenApp();
     }
 
-    // Update is called once per frame
-    void Update()
+    // set up confirmation box and pass over control
+    public void OpenConfirmationBox(InventorySlot itemSlot)
     {
-        
+        currentSlot = itemSlot; // cache the item so we can use it in the following funcs
+
+        confirmBox.SetActive(true);
+        confUseButton.onClick.AddListener(UseItem);
+        confDropButton.onClick.AddListener(DropItem);
+        confBackButton.onClick.AddListener(CloseConfirmationBox);
+
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(confUseButton.gameObject); // ensure navigation is set to the confirmation box
+    }
+
+    void UseItem()
+    {
+        currentSlot.UseItem();
+        currentSlot = null; // make sure we get rid of reference after we use the item!
+
+        CloseConfirmationBox();
+
+        Debug.Log("the item was used!");
+    }
+
+    void DropItem()
+    {
+        currentSlot.DiscardItem();
+        currentSlot = null;
+
+        CloseConfirmationBox();
+
+        Debug.Log("the item was discarded!");
+    }
+
+    // undo the confirmation box setup.
+    void CloseConfirmationBox()
+    {
+        Debug.Log("interaction finished");
+
+        confUseButton.onClick.RemoveListener(UseItem);
+        confDropButton.onClick.RemoveListener(DropItem);
+        confBackButton.onClick.RemoveListener(CloseConfirmationBox);
+
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(firstHighlightedButton);
+
+        confirmBox.SetActive(false);
     }
 }
