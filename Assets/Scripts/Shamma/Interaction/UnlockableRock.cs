aa91c9897e6c01aad24d71029795a70d8cd49a68
seapuff <s.albreiki@student.saedubai.com>
2023-08-18 16:34:53 +0400

    backing up unlockablerock script edits
    -made edits to unlockablerock to spawn dynamite obj and play beeping sound effect before spawning particles and disappearing.

--- Assets/Scripts/Shamma/Interaction/UnlockableRock.cs
+++ Assets/Scripts/Shamma/Interaction/UnlockableRock.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class UnlockableRock : PointOfInterest, IInteractableObject
@@ -9,12 +10,22 @@ public class UnlockableRock : PointOfInterest, IInteractableObject
     [SerializeField] float boxDisplayTime = 2f;
     bool noKeyfound = true;
 
-  //  [SerializeField] private Animator RockExplode;
-   // [SerializeField] private string RockExplode = "RockExplode";
 
-  //  public SphereCollider RockCollider;
+    [SerializeField] GameObject explosivesModel;
+    [SerializeField] Transform explosivesPositioning;
+
+    [SerializeField] GameObject explosionVFX;
+    [SerializeField] Transform vfxPositioning;
+
+    bool explosionSequenceActive = false;
+
+    [SerializeField] float detontationTimer = 5f;
+    [SerializeField] float lengthBetweenBeeps = 2f;
+    float timeToNextBeep;
+
 
     public AudioSource playerAudio;
+    public AudioClip audioClipForExplosiveBeep;
     public AudioClip AudioClipForRockExploding;
 
     public AudioClip NeedExplosiveVoiceOver;
@@ -36,7 +47,6 @@ public class UnlockableRock : PointOfInterest, IInteractableObject
             // check for which object is the key
             if (user.inventoryHandler.InventorySlots[i].storedItem == keyType)
             {
-                this.gameObject.name = "Explosive on Rock";
                 // use the key, make sure key-holding status is set to false if we have no more keys 
                 user.inventoryHandler.InventorySlots[i].DiscardItem();
 
@@ -47,6 +57,8 @@ public class UnlockableRock : PointOfInterest, IInteractableObject
 
                 // would want to play the door opening anim here.
 
+                explosionSequenceActive = true;
+                StartCoroutine(PlayExplosiveSequence());
               //  RockExplode.Play(RockExplode, 0, 0.0f);
              //   RockCollider.enabled = false;
 
@@ -59,7 +71,7 @@ public class UnlockableRock : PointOfInterest, IInteractableObject
 
                 Debug.Log("Rock was Blown Up!");
                 noKeyfound = false;
-                gameObject.SetActive(false);
+                
 
                 break;
             }
@@ -68,6 +80,49 @@ public class UnlockableRock : PointOfInterest, IInteractableObject
         if (noKeyfound)
         {
             StartCoroutine(DisplayKeyNeededTextbox());
+        }
+    }
+
+    IEnumerator PlayExplosiveSequence()
+    {
+        // add the explosives model, then wait for the detonation timer to expire
+        GameObject explosivesObj = GameObject.Instantiate(explosivesModel, 
+                                                   explosivesPositioning.position, 
+                                                   explosivesPositioning.rotation, 
+                                                   this.transform);
+
+        yield return new WaitForSeconds(detontationTimer);
+
+        // play explosive effect 
+
+        GameObject.Instantiate(explosionVFX, vfxPositioning.position, vfxPositioning.rotation);
+        explosionVFX.GetComponent<ParticleSystem>().Play();
+
+        explosivesObj.SetActive(false);
+        gameObject.SetActive(false); // probably want to replace this with an animation of the rocks breaking apart
+    }
+
+    void Update()
+    {
+        if (explosionSequenceActive)
+        {
+            PlayExplosiveBeep();
+        }
+    }
+
+    // should become continually faster to signify bomb is going off
+    void PlayExplosiveBeep()
+    {
+        timeToNextBeep -= Time.deltaTime;
+
+        if (timeToNextBeep <= 0)
+        {
+            playerAudio.clip = audioClipForExplosiveBeep;
+            playerAudio.Play();
+
+            timeToNextBeep = detontationTimer / lengthBetweenBeeps;
+            lengthBetweenBeeps *= 2;
+            Debug.Log("Beep");
         }
     }
 
@@ -80,18 +135,12 @@ public class UnlockableRock : PointOfInterest, IInteractableObject
         playerAudio.loop = false;
         playerAudio.Play();
 
-        StartCoroutine(TutorialManager.DisplaySubs("Damn.. I need to find something to blow up this rock...", 2.5f));
-        yield return new WaitForSeconds(2.7f);
-        StartCoroutine(TutorialManager.DisplaySubs("Maybe if I explore a bit more I could find something that might help me out with this.", 4.5f));
-
-
-
         yield return new WaitForSeconds(boxDisplayTime);
 
         keyNeededTextbox.SetActive(false);
     }
 
-    public bool IsInteractable() { return true; }
+    public bool IsInteractable() { return !explosionSequenceActive; } // shouldn't be interactable in the middle of blowing up
 
     public void OnDeHighlight() { }
 
